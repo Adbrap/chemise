b6b9ac9c77b2557c32ee6fed9ccd0265d914b490
Adbrap <adprog33@gmail.com>
2023-01-05 10:50:29 +0100

    first commit

---
+++ launcher.sh
@@ -0,0 +1,417 @@
+clear
+
+echo "                                                                         "
+echo "                                                                         "
+echo "                                                                         "
+echo "                                     ▄▄▄      ▓█████▄  ███▄ ▄███▓ ▄▄▄     ▄▄▄█████▓"
+echo "                                    ▒████▄    ▒██▀ ██▌▓██▒▀█▀ ██▒▒████▄   ▓  ██▒ ▓▒"
+echo "                                    ▒██  ▀█▄  ░██   █▌▓██    ▓██░▒██  ▀█▄ ▒ ▓██░ ▒░"
+echo "                                    ░██▄▄▄▄██ ░▓█▄   ▌▒██    ▒██ ░██▄▄▄▄██░ ▓██▓ ░ "
+echo "                                     ▓█   ▓██▒░▒████▓ ▒██▒   ░██▒ ▓█   ▓██▒ ▒██▒ ░ "
+echo "                                     ▒▒   ▓▒█░ ▒▒▓  ▒ ░ ▒░   ░  ░ ▒▒   ▓▒█░ ▒ ░░ "
+echo "                                     ▒   ▒▒ ░ ░ ▒  ▒ ░  ░      ░  ▒   ▒▒ ░   ░  "
+echo "                                    ░   ▒    ░ ░  ░ ░      ░     ░   ▒    ░   "
+echo "                                        ░  ░   ░           ░         ░  ░   "
+echo "                                             ░     "
+
+
+
+
+
+python3 /home/ubuntu/Desktop/chemise/BTC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ETH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BNB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XRP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ADA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SOL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DOGE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/MATIC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DOT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DAI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SHIB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/TRX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/AVAX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/UNI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/WBTC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LEO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LTC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ATOM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LINK.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ETC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XLM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/FTT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/NEAR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CRO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XMR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ALGO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BCH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LUNC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/QNT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/FLOW.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/VET.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/TON.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/APE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/FIL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ICP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/HBAR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/EGLD.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XTZ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/MANA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SAND.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CHZ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/EOS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/AAVE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/THETA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/AXS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/USDP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/OKB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BSV.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/TUSD.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/MKR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/KCS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ZEC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/USDD.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/HT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XEC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/MIOTA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BTT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/USDN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CAKE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/GRT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/NEO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/HNT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/KLAY.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/FTM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/PAXG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SNX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/RUNE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/NEXO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/GT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DASH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CRV.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BAT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ENJ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LDO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/STX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/KAVA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/COMP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ZIL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/FEI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/RVN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/MINA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/WAVES.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XDC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/USDTC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/GMT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/TWT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LRC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DCR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CVX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CSPR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XEM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/KSM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CELO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BTG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ENS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/1INCH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/HOT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LUNA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/RSR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BNX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/GUSD.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ROSE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/YFI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/QTUM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/AR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ANKR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/GNO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/GALA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/TFUEL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/KDA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/IOTX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/GLM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BTRST.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/JST.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CEL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/POLY.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ONE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/HIVE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/OMG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LPT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ZRX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BAL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/FLUX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/IOST.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ICX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BORA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/AMP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XYM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SRM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/KNC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ONT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/OP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/GLMR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CHSB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/STORJ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/WAXP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/WOO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ZEN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XCH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SXP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SUSHI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/IMX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/MXC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/AUDIO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/NFT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/UMA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/PLA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SKL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ABBC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SCRT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SLP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DGB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ELON.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/INJ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/VGX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CKB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CVC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/LSK.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/VLX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/PUNDIX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/COTI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/KEEP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DAO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/REN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/MED.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/RNDR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CEEK.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/REEF.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/EWT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/PEOPLE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/BTCST.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/WIN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/NU.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CELR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SYS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ONG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/XNO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/REQ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DAG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ORBS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SPELL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/RLC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/OCEAN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/API3.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/SNT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/TRIBE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ARDR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/PYR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/FX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/UOS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/POWER.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/ACA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/PROM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/DENT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/CHR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/MX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/chemise/%launcher%
+
+
